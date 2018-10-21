24501cfdc22468eddcdcbea86386ed067d52ab99
Jennifer Perez <jenniferperez@pursuit.org>
2018-10-21 11:47:17 -0400

    solved for first few questions

--- pset2.js
+++ pset2.js
@@ -5,7 +5,16 @@
     @desc - takes all engative numbers and makes positive
     @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
 */
-
+const removeNegatives=(arr)=>{
+const copyofArray=[...arr]
+    for (i=0;i<copyofArray.length;i++){
+       // Math.abs(copyofArray[i]);
+        //console.log(Math.abs(copyofArray[i]));
+        copyofArray[i]=(Math.abs(copyofArray[i]));
+    }
+    return copyofArray;
+}
+console.log(removeNegatives([-3,-2,1,2,3,-4])); // [3,2,1,2,3,4]
 /*
     @func findVal
     @param {array} arr
@@ -20,6 +29,17 @@
         findVal([1,2,3,4], 9); // -1
 */
 
+
+const findVal=(arr,val)=>{
+      for(let i = 0 ; i< arr.length; i++){
+          if(arr[i] === val){
+             return  i
+          }  
+      }
+      return -1
+    
+}
+    console.log(findVal([1,2,3,4], 2)); // 0
 /*
     @func removeOdds
     @param {array} arr
@@ -31,6 +51,19 @@
         removeOdds([1,2,3,4]); // [2,4]
         removeOdds([1,"2",3,4]); // [4]
 */
+const removeOdds=(arr)=>{
+    const copyofArray=[];
+
+    for(let i = 0 ; i< arr.length; i++)
+    {
+        if(arr[i]%2=== 0 && typeof(arr[i])==="number"){
+           copyofArray.push(arr[i]);
+        } } 
+        return copyofArray;
+    }
+
+console.log(removeOdds([1,2,3,4])); // [2,4]
+console.log(removeOdds([1,"2",3,4])); // [4]
 
 /*
     @func addSquares
@@ -40,7 +73,15 @@
     @example - 
         addSquares([1,2,3,4); // [1,2,3,4,1,4,9,16]
 */
+const addSquares=(arr)=>{
+    const copyofArray=[];
+    for(let i = 0 ; i< arr.length; i++){
+           copyofArray.push(arr[i]**2);
+    }
+    return arr.concat(copyofArray);
 
+}
+console.log(addSquares([1,2,3,4])); // [1,2,3,4,1,4,9,16])
 /*
     @func doubleify
     @param {array} arr
@@ -48,7 +89,6 @@
     @desc - take each value of array, add same value right after
     @example - doubleify([1,2,3,4); // [1,1,2,2,3,3,4,4]
 */
-
 
 /*
     @func findMax
